106708cc9c38705640882088691cc818abd7133d
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-11-16 12:06:16 +0700

    fea: implement the image controller

---
+++ src/main/java/com/hdoan/dreamshops/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package com.hdoan.dreamshops.controller;
+
+public class CategoryController {
+}
---
+++ src/main/java/com/hdoan/dreamshops/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.hdoan.dreamshops.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+
+    private String message;
+    private Object data;
+
+}
